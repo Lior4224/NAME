b0f382c47ecbcb0721fba59d4434e6341c0b53e5
jkirmayer30 <jkirmayer30@stuy.edu>
2022-01-18 12:31:39 -0500

    hyfdgudsf

--- Woo.java
+++ Woo.java
@@ -2,11 +2,12 @@ import java.io.*;
 import java.util.*;
 
 public class Woo {
-  public Plant[] farm;
+  public ArrayList<Plant> farm;
   public String name;
-  public int balance = 1000;
+  public int balance = 500;
   public int day;
   public int stamina;
+  public String currentCrop;
 
   public InputStreamReader isr;
   public BufferedReader in;
@@ -16,6 +17,33 @@ public class Woo {
     in = new BufferedReader(isr);
     newFarm()
   }
+
+  public int searchFarm(String cropName){
+    int count = 0;
+    for (int idx = 0; idx<farm.size();idx++){
+      Plant crop = farm.get(idx);
+      if (crop._name.equals(cropName)&&crop._growTime==0){
+        count++;
+      }
+    }
+    return count;
+  }
+
+  public boolean buyCrop(String name,int quantity){
+    int price = Plant.getSeedPrice(name);
+    if (balance>price*quantity){
+      balance-= price*quantity;
+      for (int i = 0 ; i<quantity;i++){
+        Plant newCrop = new Plant()
+        //fix this with methods from plant class
+        farm.add();
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 
   public void newFarm() {
     String s;
@@ -30,13 +58,23 @@ public class Woo {
 
   public void playTurn() {
     day += 1;
-    System.out.println("");
+    System.out.println("Choose the crop you want to farm today");
+    System.out.println("-wheat");
+    System.out.println("-corn");
+    System.out.println("-potato");
+    System.out.println("-beans");
+    System.out.println("-golden beans");
+    try {
+	    currentCrop =  in.readLine() ;
+    }
+    catch ( IOException e ) { }
+    System.out.println("You can buy"+(balance/Plant.getSeedPrice(currentCrop))+currentCrop);
 
   }
 
-  public String farmReport() {
-
-  }
+  // public String farmReport() {
+  //
+  // }
 
   public static void main(String[] args) {
     Woo Farm = new Woo;
